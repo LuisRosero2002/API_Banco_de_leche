a13fef947302ba2a03fee49e786928952d7945e5
Luis Rosero <luis.rosero@wiga.io>
2025-09-23 17:55:37 -0500

    Feat: se añade la entities de seguimeinto madres

--- src/entities/respuestasFriam038.entity.ts
+++ src/entities/respuestasFriam038.entity.ts
@@ -1,10 +1,17 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { PreguntasFriam038Entity } from "./preguntasFriam038.entity";
+import { VisitaSeguimientoMadresEntity } from "./visitaSeguimientoMadres.entity";
 
 @Entity({ name: "respuestas_friam_038" })
 export class RespuestasFriam038Entity {
     @PrimaryGeneratedColumn("increment", { name: "id_respuesta" })
     id!: number;
     @Column({ name: "respuesta", type: "text", nullable: false })
-    respuesta!: string;     
-
+    respuesta!: string;
+    @ManyToOne(() => PreguntasFriam038Entity, pregunta => pregunta.respuestas)
+    @JoinColumn({ name: "id_pregunta" })
+    pregunta!: PreguntasFriam038Entity;
+    @ManyToOne(() => VisitaSeguimientoMadresEntity, visitaSeguimiento => visitaSeguimiento.respuestas)
+    @JoinColumn({ name: "id_visita_domiciliario" })
+    visitaSeguimiento!: VisitaSeguimientoMadresEntity;
 }
--- src/entities/visitaSeguimientoMadres.entity.ts
+++ src/entities/visitaSeguimientoMadres.entity.ts
@@ -1,4 +1,7 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { MadresDonantesEntity } from "./madresDonantes.entity";
+import { DatosVisitaSeguimientoEntity } from "./datos_visita_seguimiento.entity";
+import { RespuestasFriam038Entity } from "./respuestasFriam038.entity";
 
 @Entity({ name: "visita_seguimiento_friam_038" })
 export class VisitaSeguimientoMadresEntity {
@@ -6,13 +9,12 @@ export class VisitaSeguimientoMadresEntity {
     id!: number;
     @Column({ name: "fecha", type: "date", nullable: false })
     fecha!: Date;
-    @Column({ name: "observaciones", type: "text", nullable: true })
-    observaciones!: string;
-    @Column({ name: "recomendaciones", type: "text", nullable: true })
-    recomendaciones!: string;
-    @Column({ name: "firma_usuario", type: "text", nullable: true })
-    firmaUsuario!: string;
-    @Column({ name: "firma_evaluador", type: "text", nullable: true })
-    firmaEvaluador!: string;
 
+    @ManyToOne(() => MadresDonantesEntity, madreDonante => madreDonante.visitaSeguimiento)
+    @JoinColumn({ name: "id_visita_seguimiento" })
+    madreDonante!: MadresDonantesEntity;
+    @OneToOne(() => DatosVisitaSeguimientoEntity, datosVisitaSeguimiento => datosVisitaSeguimiento.visitaSeguimiento)
+    datosVisitaSeguimiento!: DatosVisitaSeguimientoEntity;
+    @OneToMany(() => RespuestasFriam038Entity, respuesta => respuesta.visitaSeguimiento)
+    respuestas!: RespuestasFriam038Entity[];
 } 
