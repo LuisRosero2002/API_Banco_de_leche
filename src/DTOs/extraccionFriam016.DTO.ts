06cc280796ee82e357c2b402938c251a4f85ebc0
Luis Rosero <luis.rosero@wiga.io>
2025-10-28 23:47:57 -0500

    feat: modificaciones API actualizar

--- src/DTOs/extraccionFriam016.DTO.ts
+++ src/DTOs/extraccionFriam016.DTO.ts
@@ -20,3 +20,44 @@ export class ExtraccionFriam016DTO {
     @IsOptional()
     observaciones!: string;
 }
+
+export class FrascosExtraccionPutDTO {
+    @IsNotEmpty()
+    id_registro_extraccion!: number;
+    @IsNotEmpty()
+    fecha!: Date;
+    @IsNotEmpty()
+    fecha_display!: string;
+    @IsNotEmpty()
+    extraccion_1!: Extraccion1;
+    @IsNotEmpty()
+    extraccion_2!: Extraccion2;
+    @IsNotEmpty()
+    motivo_consulta!: string;
+    @IsNotEmpty()
+    observaciones!: string;
+    @IsNotEmpty()
+    fecha_aux!: Date;
+}
+
+export class Extraccion1 {
+    @IsOptional()
+    id!: number;
+    @IsNotEmpty()
+    am!: string;
+    @IsNotEmpty()
+    ml!: number;
+    @IsNotEmpty()
+    am_aux!: Date;
+}
+
+export class Extraccion2 {
+    @IsOptional()
+    id!: number;
+    @IsNotEmpty()
+    pm!: string;
+    @IsNotEmpty()
+    ml!: number;
+    @IsNotEmpty()
+    pm_aux!: Date;
+}
